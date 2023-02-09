a0680f1ce4a6cc65d98f0d516683eff80df385a9
Nurlat Bekdullayev <natuspati@gmail.com>
2023-02-09 20:15:22 +0600

    Create Region and Country tables.

--- GuessCountry/admin.py
+++ GuessCountry/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from GuessCountry.models import Region, Country
 
 # Register your models here.
+admin.site.register(Region)
+admin.site.register(Country)
--- GuessCountry/models.py
+++ GuessCountry/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+
 
 # Create your models here.
+class Region(models.Model):
+    REGIONS = (
+        ('EU', 'Europe'),
+        ('AS', 'Asia'),
+        ('AF', 'Africa'),
+        ('OC', 'Oceania'),
+        ('AQ', 'Antarctic'),
+    )
+    
+    name = models.CharField(max_length=50, choices=REGIONS)
+    
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    capital = models.CharField(max_length=100)
+    region = models.ForeignKey(Region, on_delete=models.PROTECT)
+    population = models.IntegerField(validators=[MinValueValidator(0)])
+    flag = models.URLField(max_length=200)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Country'
+        verbose_name_plural = 'Countries'
+        
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+    
+    def __str__(self):
+        return self.name
